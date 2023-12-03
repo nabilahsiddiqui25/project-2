508d25c8d080c86ea8639bad5210c1f028a5aed9
Jlrod3 <112913660+Jlrod3@users.noreply.github.com>
2023-12-02 18:08:18 -0600

    js script for sliding menu

--- js/site.js
+++ js/site.js
@@ -7,9 +7,11 @@ jQuery(function($) {
 
 window.onload = function() {
   const menu_btn = document.querySelector(".hamburger");
+  const mobile_menu = document.querySelector(".mobile-nav");
 
   menu_btn.addEventListener("click", function () {
     menu_btn.classList.toggle("is-active");
+    mobile_menu.classList.toggle("is-active");
   });
 }
 
